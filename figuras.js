7e1a84bd89dbabfd61a7676e0d21607a4faa4fa2
Juanjo Servin <juanjoservin2@hotmail.com>
2021-09-14 09:58:21 -0400

    Agregamos HTML y JavaScript

---
+++ figuras.js
@@ -0,0 +1,20 @@
+//Código del cuadrado
+console.group("Cuadrados");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado +"cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado +"cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es; " + areaCuadrado + "cm2");
+
+//Código del triángulo
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTriangulo1 = 4;
+console.log("Los lados del triángulo miden: "
++ ladoTriangulo1+"cm, " 
++ ladoTriangulo2+"cm, "
++ baseTriangulo + "cm");
+
